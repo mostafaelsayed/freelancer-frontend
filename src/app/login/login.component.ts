500099a319ac95cd13813e41211f8f440287dd21
mostafaelsayed <mostafa.elsayed@ibm.com>
2019-06-21 19:01:07 +0200

    add dev env and login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  environmentName = '';
+  apiUrl = '';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    environmentName: 'dev',
+    apiUrl: 'freelancer-backend.w3ibm.mybluemix.net'
+};
+  
