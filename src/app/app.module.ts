aa2c18462a7e5b581b52dd218b5322702320f005
mostafaelsayed <mostafa.elsayed@ibm.com>
2019-06-22 14:22:18 +0200

    enable login logic and add user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +10,21 @@ import { environment } from '../../environments/environment';
 })
 export class LoginComponent implements OnInit {
 
-  environmentName = '';
-  apiUrl = '';
+  // logged in user object
+  user: User = {
+    email: '',
+    password: ''
+  };
 
-  constructor() {
+  apiUrl = environment.apiUrl;
 
+  login(): void {
+    this.http.post(`${this.apiUrl}/api/login`, this.user).subscribe((res) => {
+      console.log(res);
+    });
   }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
